1647f278fb9391fc194798b1722299eaadc5d7d5
Anas ESSAHL <ESSAHL.Anas@ofppt-edu.ma>
2020-05-05 06:08:36 +0000

    as

--- PFM/PFM/Models/ApplicationDbContext.cs
+++ PFM/PFM/Models/ApplicationDbContext.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using PFM.Models.ModelsReservation;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace PFM.Models
 {
@@ -17,5 +23,62 @@ namespace PFM.Models
         public virtual DbSet<Countries> Countries { get; set; }
         public virtual DbSet<States> States { get; set; }
         public virtual DbSet<Cities> Cities { get; set; }
+
+        public virtual DbSet<Room> Rooms { get; set; }
+        public virtual DbSet<RoomImage> RoomImages { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Room Model sets
+            modelBuilder.Entity<Room>().HasKey(r => r.ChambreId);
+            modelBuilder.Entity<Room>().Property(r => r.Titre).HasMaxLength(255).IsRequired();
+            modelBuilder.Entity<Room>().Property(r => r.ImageId).IsOptional();
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            //Images Model sets
+            modelBuilder.Entity<RoomImage>()
+                .Property(ri => ri.Name)
+                .HasMaxLength(255);
+
+            modelBuilder.Entity<RoomImage>()
+                .HasKey(ri => ri.ImageId);
+
+            modelBuilder.Entity<RoomImage>().HasRequired(r => r.Room)
+                .WithMany(ri => ri.RoomImages)
+                .HasForeignKey(ri => ri.RoomId);
+
+
+
+
+            modelBuilder.Entity<IdentityUserRole>()
+                .HasKey(u => new { u.RoleId, u.UserId }).ToTable("AspNetUserRoles");
+
+            modelBuilder.Entity<IdentityUserLogin>()
+                .HasKey(l => new { l.LoginProvider, l.ProviderKey, l.UserId })
+            .ToTable("AspNetUserLogins");
+
+            //Reservation
+
+            modelBuilder.Entity<Reservation>().HasRequired(r => r.Room)
+           .WithMany(r => r.Reservations)
+           .HasForeignKey(ri => ri.RoomId);
+
+            //Caracteristiques
+
+            modelBuilder.Entity<Caracteristique>().HasKey(c => c.CaracId);
+
+            modelBuilder.Entity<Caracteristique>().HasRequired(r => r.Room)
+            .WithMany(r => r.Caracteristiques)
+            .HasForeignKey(ri => ri.RoomId);
+
+        }
+
+        public System.Data.Entity.DbSet<PFM.Models.ModelsReservation.Reservation> Reservations { get; set; }
+
+        public System.Data.Entity.DbSet<PFM.Models.ModelsReservation.Caracteristique> Caracteristiques { get; set; }
     }
 }
---
+++ PFM/PFM/Models/ModelsReservation/Reservation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PFM.Models.ModelsReservation
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+
+        public int RoomId { get; set; }
+
+        public int UserId { get; set; }
+        public DateTime DateDebut { get; set; }
+        public DateTime DateFin { get; set; }
+        public string Confirmation { get; set; }
+
+        public int NbChambres { get; set; }
+       
+        public virtual Room Room { set; get; }
+    }
+}
